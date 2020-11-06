5c17bb4a1611b6eee26257b2c0a2649c85e17da3
EdgarCortez1 <ea.cortezz@gmail.com>
2020-11-06 16:40:36 -0600

    completed showTextNode, showOption, and selectOption functions

--- js/game.js
+++ js/game.js
@@ -1,24 +1,45 @@
-const textElement = document.getElementById( 'message' );
-const optionButtonsElement = document.getElementById( 'option-buttons' );
+const textElement = document.getElementById('message');
+const optionButtonsElement = document.getElementById('option-buttons');
 
 let state = {}
 
 function startGame() {
-  state = {}
-  showTextNode( 1 )
+  state = {}; // make sure the state is empty
+  showTextNode(1); // loads the first text node
 }
 
-function showTextNode( textNodeIndex ) {
-  const textNode = textNodes.find( textNode => textNode.id === textNodeIndex )
-  textElement.innerText = textNode.text
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex); // finds the object with the id given
+  textElement.innerText = textNode.text; // uses the text from the found object
+
+  // removes all options
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild);
+  }
+
+  // adds all options available from the text node object 
+  textNode.options.forEach(option => {
+    if (showOption(option)) {
+      const button = document.createElement('button');
+      button.innerText = option.text;
+      button.classList.add('btn');
+      button.addEventListener('click', () => selectOption(option));
+      optionButtonsElement.appendChild(button);
+    }
+  })
 }
 
-function showOption( option ) {
-
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state);
 }
 
-function selectOption( option ) {
-
+function selectOption(option) {
+  const nextTextNode = option.nextText;
+  if (nextTextNode <= 0) {
+    return startGame();
+  }
+  state = Object.assign(state, option.setState);
+  showTextNode(nextTextNode);
 }
 
 startGame()
